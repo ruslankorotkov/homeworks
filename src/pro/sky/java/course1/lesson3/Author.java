f31dfcd8c41e2319cdcea3344c35e4e3df72ae07
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-20 19:54:18 +0300

    Объекты и классы.задание 1.2.3.

---
+++ src/pro/sky/java/course1/lesson3/Author.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course1.lesson3;
+
+public class Author {
+    String publisherName;
+    String bookName;
+    int publishingYear;
+    public Author (String authorName,String bookName,int publishingYear) {
+        this.publisherName = authorName;
+        this.bookName = bookName;
+        this.publishingYear = publishingYear;
+    }
+}
