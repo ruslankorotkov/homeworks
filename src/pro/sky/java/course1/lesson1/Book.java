788ec41c210baf4df2a0de7a7daff0b229502a2b
ruslankorotkov <ruslankorotkov.rus@yandex.ru>
2022-12-20 17:14:49 +0300

    Объекты и классы.задание 1.

---
+++ src/pro/sky/java/course1/lesson1/Book.java
@@ -0,0 +1,17 @@
+package pro.sky.java.course1.lesson1;
+
+public class Book {
+    public static void main(String[] args) {
+        String bookNameGogol = " Тарас Бульба ";
+        String authorNameGogol = " Николай Гоголь ";
+        int publishingYearGogol = 1842;
+        String bookNameSholohov = " Судьба человека ";
+        String authorNameSholohov = " Михаил Шолохов ";
+        int publishingYearSholohov = 1957;
+        System.out.println(authorNameSholohov);
+    }
+}
+
+
+
+
